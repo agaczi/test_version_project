bc35b8b019831c02b40a57a128cbd3e986a45d04
Agata Maciejewska <aamaciejewska@gmail.com>
2016-01-26 04:55:20 +0100

    including js to the partners section

--- js/app.js
+++ js/app.js
@@ -1,3 +1,20 @@
 /**
  * Created by Agata on 23.01.2016.
  */
+//PARTNERS SECTION
+
+var overlay = $('.overlay');
+var gallery = $('.gallery');
+
+overlay.hide();
+
+function showShadow() {
+    $(gallery).mouseenter(function () {
+        $(this).next(overlay).show();
+    });
+    $(overlay).mouseleave(function () {
+        overlay.hide();
+    });
+}
+
+showShadow();
